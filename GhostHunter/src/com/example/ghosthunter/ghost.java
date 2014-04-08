f8e4fc6700a2683443d09de9a1d69840ab6b71fe
baw8cp <baw8cp@virginia.edu>
2014-04-08 18:46:00 -0400

    take 2

---
+++ GhostHunter/src/com/example/ghosthunter/ghost.java
@@ -0,0 +1,14 @@
+package com.example.ghosthunter;
+
+import android.content.Context;
+import android.view.View;
+
+public class ghost extends View{
+
+	/**Constructor*/
+	public ghost(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ GhostHunter/src/com/example/ghosthunter/hunter.java
@@ -0,0 +1,16 @@
+package com.example.ghosthunter;
+
+import android.content.Context;
+import android.view.View;
+
+public class hunter extends View{
+
+	/**Constructor*/
+	public hunter(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
